3462a3a569cea0ffbc19da8eb5d1815f0335adda
unknown <joseparanan@gmail.com>
2021-03-23 15:48:25 -0300

    1.0

---
+++ eCommerce/src/main/java/com/farmacia/eCommerce/model/Produto.java
@@ -0,0 +1,80 @@
+package com.farmacia.eCommerce.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_produto")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull
+	private String nome;
+	
+	@NotNull
+	@Digits(integer = 10, fraction = 2)
+	private BigDecimal preco;
+	
+	@NotNull
+	private long qtdEstoque;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public long getQtdEstoque() {
+		return qtdEstoque;
+	}
+
+	public void setQtdEstoque(long qtdEstoque) {
+		this.qtdEstoque = qtdEstoque;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+
+}
